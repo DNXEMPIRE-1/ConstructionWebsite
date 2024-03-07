b1dc1b945368b59bd347592399d9ebacf210a0fd
“Dennis <dennisnzioki019@gmail.com>
2024-03-07 18:53:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Sample data for portfolio items
 const portfolioData = [
-    { category: "painting", image: "painting1.jpg", title: "Painting 1" },
-    { category: "painting", image: "painting2.jpg", title: "Painting 2" },
+    { category: "building-construction", image: "img/conpaint.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con1.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con2.jpg", title: "Construction" },
+    { category: "fixing-support", image: "img/fix.jpg", title: "fixing-support" },
+    { category: "building-construction", image: "img/con3.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con4.jpg", title: "Construction" },
+    { category: "architecture-design", image: "img/int1.jpg", title: "architecture-design" },
+    { category: "painting", image: "img/conpaint.jpg", title: "Painting" },
+    { category: "house-renovation", image: "img/hr.jpg", title: "house-renovation" },
+    { category: "house-renovation", image: "img/hr2.jpg", title: "house-renovation" },
+    { category: "house-renovation", image: "img/hr3.jpg", title: "house-renovation" },
+    { category: "building-construction", image: "img/con5.jpg", title: "Construction" },
+    { category: "architecture-design", image: "img/ad.jpg", title: "architecture-design" },
+    { category: "building-construction", image: "img/con.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con6.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con7.jpg", title: "Construction" },
+    { category: "building-construction", image: "img/con8.jpg", title: "Construction" },
+    { category: "architecture-design", image: "img/ad2.jpg", title: "architecture-design" },
+    { category: "building-construction", image: "img/con9.jpg", title: "Construction" },
+    { category: "architecture-design", image: "img/ad3.jpg", title: "architecture-design" },
+    { category: "building-construction", image: "img/con10.jpg", title: "Construction" },
+    { category: "interior-design", image: "img/id.jpg", title: "interior-design" },
+    { category: "architecture-design", image: "img/ad4.jpg", title: "architecture-design" },
+    { category: "building-construction", image: "img/con11.jpg", title: "Construction" },
+    { category: "painting", image: "img/paint1.jpg", title: "Painting" },
+    { category: "building-construction", image: "img/con12.jpg", title: "Construction" },
+    { category: "architecture-design", image: "img/ad5.jpg", title: "architecture-design" },
+
+
+
     // Add more data for other categories...
 ];
